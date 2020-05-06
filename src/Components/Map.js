002db6af785a46d9a550b002872d75919b8ce8b7
quoct <quoctoan.tn06@gmail.com>
2020-05-06 17:51:12 +0700

    Test MapView and Polyline

--- src/Components/Map.js
+++ src/Components/Map.js
@@ -1,14 +1,41 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import MapView, { Polyline } from "react-native-maps";
 
 const Map = () => {
+    let points = [];
+
+    for (let i = 0; i < 20; i++) {
+        if (i % 2 === 0) {
+            points.push({
+                latitude: 37.33233 + i * 0.001,
+                longitude: -122.03121 + i * 0.001,
+            });
+        } else {
+            points.push({
+                latitude: 37.33233 - i * 0.001,
+                longitude: -122.03121 + i * 0.001,
+            });
+        }
+    }
     return (
-        <View>
-            <Text>Map</Text>
-        </View>
-    )
-}
+        <MapView
+            style={styles.map}
+            initialRegion={{
+                latitude: 37.33233,
+                longitude: -122.03121,
+                latitudeDelta: 0.01,
+                longitudeDelta: 0.01,
+            }}>
+            <Polyline coordinates={points} />
+        </MapView>
+    );
+};
 
-export default Map
+export default Map;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    map: {
+        height: 300,
+    },
+});
